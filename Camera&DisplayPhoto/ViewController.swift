69a544a4606de46b1e8c9bc5fdc83a9ed3e10f9a
Katie <katiehsam@gmail.com>
2022-06-14 10:40:24 -0700

    code

--- Camera&DisplayPhoto/ViewController.swift
+++ Camera&DisplayPhoto/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    
+    var imagePicker = UIImagePickerController()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func SelfiePressed(_ sender: Any) {
+        
+        imagePicker.sourceType = .camera
+         
+    }
+    
+    
+    
+    @IBAction func ChoosePhotoTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    
+    
 
 }
 
